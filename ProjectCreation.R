18eec76b96188bfc6bf4ecb3e5f4b9fc89bfe8a0
Joshua Aldrich <jbot.aldrich@gmail.com>
2013-11-01 11:31:33 -0700

    Removed unnecessary curl command.  Added the file download to projecttemplate command.

--- ProjectCreation.R
+++ ProjectCreation.R
@@ -9,23 +9,13 @@ projtemplate <- function(directory)
   dir.create(file.path(getwd(), "Output"), showWarnings = FALSE)
   dir.create(file.path(getwd(), "R"), showWarnings = FALSE)
   dir.create(file.path(getwd(), "Doc"), showWarnings = FALSE)
+  download.file("https://raw.github.com/jbotaldrich/iTRAQTools/master/PlotUtils.R", file.path(directory, "R/Plotutils.R"))
+  download.file("https://raw.github.com/jbotaldrich/iTRAQTools/master/AnalysisSource.R", file.path(getwd(), "R/AnalysisSource.R"))
+  download.file("https://raw.github.com/jbotaldrich/iTRAQTools/master/TableGeneration.R", file.path(directory, "R/TableGeneration.R"))
+  download.file("https://raw.github.com/jbotaldrich/iTRAQTools/master/Utilities.R", file.path(directory, "R/Utilities.R"))
+  
 }
 
-source_github <- function(u) {
-  # load package
-  require(RCurl)
-  
-  # read script lines from website
-  script <- getURL(u, ssl.verifypeer = FALSE)
-  
-  # parase lines and evealuate in the global environement
-  eval(parse(text = script))
-}
-
-download.file("https://raw.github.com/jbotaldrich/iTRAQTools/master/PlotUtils.R", file.path(getwd(), "R/Plotutils.R"))
-download.file("https://raw.github.com/jbotaldrich/iTRAQTools/master/AnalysisSource.R", file.path(getwd(), "R/AnalysisSource.R"))
-download.file("https://raw.github.com/jbotaldrich/iTRAQTools/master/TableGeneration.R", file.path(getwd(), "R/TableGeneration.R"))
-download.file("https://raw.github.com/jbotaldrich/iTRAQTools/master/Utilities.R", file.path(getwd(), "R/Utilities.R"))
 
 
 
